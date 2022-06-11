7a449b4709fb8901b57d3b78ff635e4f0ab0a421
Sarah H <sarahpontoh02@gmail.com>
2022-06-11 22:28:26 +0700

    tes

--- koneksi.js
+++ koneksi.js
@@ -3,12 +3,19 @@ var mysql = require('mysql');
 // Create a connection
 var conn = 
   mysql.createConnection({
+    // host: "sql6.freemysqlhosting.net", 
+    // port: "3306",
+    // user: "sql6499049", 
+    // password: "jnSzNvMWUa",
+    // database: "sql6499049",
+    // multipleStatements: true
+
+    //cherry
     host: "sql6.freemysqlhosting.net", 
     port: "3306",
-    user: "sql6499049", 
-    password: "jnSzNvMWUa",
-    database: "sql6499049",
-    multipleStatements: true
+    user: "sql6498933", 
+    password: "6tIXSJsPIZ",
+    database: "sql6498933"
   });
 
 conn.connect(function(err, conn){
