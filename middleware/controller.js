0b23a1f4bf7765973b29e108c26334298d20b153
Sarah H <sarahpontoh02@gmail.com>
2022-06-11 22:45:53 +0700

    bismillah Allahuakbar

---
+++ middleware/controller.js
@@ -0,0 +1,284 @@
+'use strict';
+
+var response = require('../res');
+const conn = require('../koneksi');
+var mysql = require('mysql');
+var md5 = require('MD5');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+var ip = require('ip');
+
+var parsetoken = require('./parseJWT'); //used once already logged in
+
+// //GET home
+// exports.index = function(req, res){
+//     response.success("Running", res)
+// };
+
+//POST register
+exports.register = function(req, res){
+    //req
+    var post = {
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password,   //md5(req.body.password)
+        role : '2', //blank
+        balance : req.body.balance
+    }
+                                         
+    var query = "SELECT email FROM users WHERE ?? = ?"; //double "??" for sql query, single "?" for variable
+    var table = ["email", post.email];
+    query = mysql.format(query, table);
+
+    conn.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else{
+            if(rows.length == 0){   //post.email is not found in db
+                var query = "INSERT INTO ?? SET ?";     
+                var table = ["users"];
+                query = mysql.format(query, table);
+
+                conn.query(query, post, function(error, rows){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        response.success("Registered", res);
+                    }
+                });
+            }else{
+                response.failed("Email is already registered", res); //else if found
+            }
+        }
+    });
+};
+
+//POST login
+exports.login = function(req, res){
+    //req
+    var post = {
+        email : req.body.email,
+        password : req.body.password
+    }
+
+    var query = "SELECT * FROM users WHERE ?? = ? AND ?? = ?";
+    var table = ["password", post.password, "email", post.email];   // md5(post.password) if hashed
+    query = mysql.format(query, table);
+
+    conn.query(query, function(error, rows){
+        
+        console.log("test");
+        if(error){
+             
+            // response.success("error", res);
+        }else{
+            if(rows.length == 1){
+                var token = jwt.sign({rows}, config.secret, {
+                    expiresIn: 1440
+                });
+
+                var user_id = rows[0].id;
+                var data = {
+                    user_id: user_id,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                // var query = "INSERT INTO ?? SET ?";
+                // var table = ["token"];
+                // query = mysql.format(query, table);
+
+                // conn.query(query, data, function(error, rows){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        res.json({
+                            success: true,
+                            message: "Login successful",
+                            token: token,
+                            currUser: data.user_id
+                        });
+                    }
+                // });
+            }else{
+                res.json({
+                    "error": true,
+                    "message": "Incorrect password or email"
+                });
+
+            }
+        }
+    });
+};
+
+
+//PUT user topup
+exports.topup = function(req, res){
+    //req
+    var token = req.headers.authorization;
+    var topup = req.body.balance;
+
+    var parsedtoken = parsetoken(token);
+    var data = parsedtoken.rows[0];
+
+    var id = data.id;
+
+    conn.query('UPDATE users SET balance = balance + ? WHERE id = ?', [topup, id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.success("Topup successful", res);
+            }
+        });
+};
+
+//PUT transfer
+exports.transfer = function(req, res){
+    //req
+    var receiver_email = req.body.email; //receiver's email
+    var amount = req.body.balance;
+    var token = req.headers.authorization;
+
+    var parsedtoken = parsetoken(token);
+    var data = parsedtoken.rows[0];
+    
+    var sender_id = data.id;
+    var sender_name = data.name;
+    
+    conn.query('SELECT id, name FROM users WHERE email = ?', [receiver_email], function(error, rows, fields){
+        var receiver_id = rows[0].id;
+        var receiver_name = rows[0].name;
+        
+        conn.query('UPDATE users SET balance = balance - ? WHERE id = ? ', 
+            [amount, sender_id],
+            function(error, rows, fields){
+                if(error){
+                    console.log(error);
+                }else if(sender_id == receiver_id){
+                    response.failed("You cannot transfer to yourself", res);
+                }else{
+                    response.success("Transfered successfully", res);
+                }
+        });
+
+        conn.query('UPDATE users SET balance = balance + ? WHERE id = ? ', 
+            [amount, receiver_id],
+            function(error, rows, fields){
+                if(error){
+                    console.log(error);
+                }else if(sender_id == receiver_id){
+                    response.failed("You cannot transfer to yourself", res);
+                }else{
+                    response.success("Transfered successfully", res);
+                }
+        });
+
+        conn.query('INSERT INTO transactions (sender_id,receiver_id,sender,receiver,amount) VALUES (?,?,?,?,?)', 
+        [sender_id, receiver_id, sender_name, receiver_name, amount],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else if(sender_id == receiver_id){
+                response.failed("You cannot transfer to yourself", res);
+            }else{
+                response.success("Transfered successfully", res);
+            }
+        });
+    })
+
+};
+
+
+//GET profile
+exports.profile = function(req, res){
+    //req
+    var token = req.headers.authorization;
+
+    var parsedtoken = parsetoken(token);
+    var data = parsedtoken.rows[0];
+
+    var id = data.id;
+    
+    conn.query('SELECT * FROM users WHERE id = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                res.json({
+                    "id": data.id,
+                    "name": data.name,
+                    "email": data.email,
+                    "password": data.password,
+                    "balance": data.balance
+                });
+                // console.log(data);
+                // res.json(data);
+                // response.success(data, res);
+            }
+    });
+    
+  
+};
+
+
+
+
+
+
+
+
+// //GET all record
+// exports.allusers = function(req, res){
+//     conn.query('SELECT * FROM users', function(error, rows, fields){
+//         if(error){
+//             conn.log(error);
+//         }else{
+//             response.success(rows, res)
+//         }
+//     });
+// };
+
+// //GET one user
+// exports.user = function(req, res){
+//     let id = req.params.id;  //taro di parameter   
+
+//     conn.query('SELECT * FROM users WHERE id = ?', [id],
+//         function(error, rows, fields){
+//             if(error){
+//                 console.log(error);
+//             }else{
+//                 response.success(rows, res);
+//             }
+
+//     });
+
+// };
+
+// //POST Register
+// exports.register = function (req, res){
+//     var name = req.body.name;
+//     var email = req.body.email;
+//     var password = req.body.password;
+//     var role = req.body.role;
+//     var balance = req.body.balance;
+
+//     conn.query('INSERT INTO users (name,email,password,role,balance) VALUES (?,?,?,2,?)',
+//         [name, email, password, balance],
+//         function(error, rows, fields){
+//             if(error){
+//                 console.log(error);
+//             }else{
+//                 response.success("Registered", res);
+//             }
+//         });
+
+// };
+
+
+
+
+
+
+
+
--- middleware/index.js
+++ middleware/index.js
@@ -1,50 +1,50 @@
 var express = require('express');
-var auth = require('./auth');
+// var auth = require('./auth');
 var router = express.Router();
 var verifikasi = require('./verifikasi')
-var jsonku = require('./logged_in');
-// var controller = require('./controller');
+// var jsonku = require('./logged_in');
+var controller = require('./controller');
 
-// Registrasi
-router.post('/profile', auth.registrasi);
+// // Registrasi
+// router.post('/profile', auth.registrasi);
 
-// Login
-router.post('/login', auth.login);
+// // Login
+// router.post('/login', auth.login);
 
-// GET profile - Sebelumnya: saldo
-router.get('/profile', verifikasi(), jsonku.tampilSaldo);
+// // GET profile - Sebelumnya: saldo
+// router.get('/profile', verifikasi(), jsonku.tampilSaldo);
 
-// PUT profile - Sebelumnya: top up
-router.put('/profile/:id_user', verifikasi(), jsonku.topUp);
+// // PUT profile - Sebelumnya: top up
+// router.put('/profile/:id_user', verifikasi(), jsonku.topUp);
 
-// POST pembelian - Sebelumnya: bayar
-router.post('/pembelian', verifikasi(), jsonku.bayar);
+// // POST pembelian - Sebelumnya: bayar
+// router.post('/pembelian', verifikasi(), jsonku.bayar);
 
-// GET pembelian - melihat history pembayaran
-router.get('/pembelian/:id_user', verifikasi(), jsonku.cekPembayaran);
-router.get('/pembelian/:id_user/:id_pembelian', verifikasi(), jsonku.cekIDPembayaran);
+// // GET pembelian - melihat history pembayaran
+// router.get('/pembelian/:id_user', verifikasi(), jsonku.cekPembayaran);
+// router.get('/pembelian/:id_user/:id_pembelian', verifikasi(), jsonku.cekIDPembayaran);
 
-// POST transaksi - untuk melakukan pembayaran
-router.post('/transaksi', jsonku.transaksi)
+// // POST transaksi - untuk melakukan pembayaran
+// router.post('/transaksi', jsonku.transaksi)
 
-// POST Transfer
-router.post('/transfer', verifikasi(), jsonku.transfer);
+// // POST Transfer
+// router.post('/transfer', verifikasi(), jsonku.transfer);
 
-// GET History
-router.get('/riwayat', verifikasi(), jsonku.history);
-
-
+// // GET History
+// router.get('/riwayat', verifikasi(), jsonku.history);
 
 
 
 
-// // router.get('/', controller.index);
-// router.post('/register', controller.register);
-// router.post('/login', controller.login);
 
-// //Authentication
-// router.put('/topup', verifikasi(), controller.topup)
-// router.put('/transfer', verifikasi(), controller.transfer)
-// router.get('/profile', verifikasi(), controller.profile)
+
+// router.get('/', controller.index);
+router.post('/register', controller.register);
+router.post('/login', controller.login);
+
+//Authentication
+router.put('/topup', verifikasi(), controller.topup)
+router.put('/transfer', verifikasi(), controller.transfer)
+router.get('/profile', verifikasi(), controller.profile)
 
 module.exports = router;
