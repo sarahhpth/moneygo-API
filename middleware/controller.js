1ad7ab21c4bd6857b6b9b57f9731419603cd6fe4
Sarah H <sarahpontoh02@gmail.com>
2022-06-12 05:49:36 +0700

    coba

--- middleware/controller.js
+++ middleware/controller.js
@@ -118,19 +118,19 @@ exports.topup = function(req, res){
     var topup = req.body.balance;
 
     var parsedtoken = parsetoken(token);
-    console.log(parsedtoken);
+    // console.log(parsedtoken);
     // var data = parsedtoken.rows[0];
+    var data = parsedtoken.rows;
+    var id = data.id;
 
-    // var id = data.id;
-
-    // conn.query('UPDATE users SET balance = balance + ? WHERE id = ?', [topup, id],
-    //     function(error, rows, fields){
-    //         if(error){
-    //             console.log(error);
-    //         }else{
-    //             response.success("Topup successful", res);
-    //         }
-    //     });
+    conn.query('UPDATE users SET balance = balance + ? WHERE id = ?', [topup, id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.success("Topup successful", res);
+            }
+        });
 };
 
 //PUT transfer
